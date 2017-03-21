2d0c947237749c39c3f6d350579c4b27fde23249
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-03-21 13:02:18 -0400

    Gather movie description

--- couch_potato/main/models.py
+++ couch_potato/main/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.db.models.signals import post_init
+
+from .tmdb import search_for_movie
 
 # Create your models here.
 
 class Movie(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default="unnamed")
     number = models.IntegerField(default=0)
+    description = models.CharField(max_length=200, default="undescribed")
     
     def __str__(self):
         return "Name: " + self.name + " Number: " + str(self.number)
     
+def intialize_movie(**kwargs):
+    instance = kwargs.get('instance')
+    options = search_for_movie(instance.name)
+
+    if len(options) > 0:
+        instance.description = options[0]["overview"]
+    else:
+        instance.description = "Unable to locate description for this movie."
+
+post_init.connect(intialize_movie, Movie)
