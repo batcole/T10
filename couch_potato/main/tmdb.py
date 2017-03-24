602d4ea9cacf2691504d956e4b228d0bdde6dd35
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-03-24 15:42:52 -0400

    Use tmdbsimple instead of requests

--- couch_potato/main/tmdb.py
+++ couch_potato/main/tmdb.py
@@ -7,33 +7,14 @@
 
 import requests
 import json
-
-# In these strings, %s will be replaced by the movie number.
-# for example, "test %s" % "injected value" will result in "test injected value"
-
-BASE_URL = "https://api.themoviedb.org/3"
-ID_URL = BASE_URL + "/movie/%s"
-SEARCH_URL = BASE_URL + "/search/movie"
-
-def movie_data_by_id(movie_id):
-    params = {"api_key":"74b5f1b300deec307cbe4df5228627a6"}
-
-    response = requests.request("GET", ID_URL % movie_id, params=params)
-    if response.status_code != 200:
-        return []
-    
-    data = json.loads(response.text)
-    return data
+import tmdbsimple as tmdb
+from .keys import API_KEY
+tmdb.API_KEY = API_KEY
 
 def search_for_movie(movie_name):
-    params = {"api_key":"74b5f1b300deec307cbe4df5228627a6",
-              "query": movie_name
-              }
-    
-    response = requests.request("GET", SEARCH_URL, params=params)
-    if response.status_code != 200:
+    search = tmdb.Search()
+    if movie_name == "":
         return []
-
-    results = json.loads(response.text)["results"]
+    results = search.movie(query=movie_name)["results"]
     top_three = results[:3]
     return top_three
