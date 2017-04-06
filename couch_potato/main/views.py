5db5b39efc27690dc37d12086d0c5d066dbe27a5
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-04-06 13:43:02 -0400

    starting the backend for search button

--- couch_potato/main/views.py
+++ couch_potato/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.db.models import Q
 
 from .models import Movie
 
@@ -11,6 +12,11 @@ def detail(request, movie_id):
     return render(request, 'main/detail.html', {'movie': movie})
 
 def index(request):
+    if(request.GET.get('submitMovie')):
+        search_query = request.GET.get('search_box', None)
+        print(search_query)
+        results = Movie.objects.filter(Q(name__icontains=search_query))
+        print("search results:", results)
     movie_list = Movie.objects.all()
     context = {'movie_list': movie_list}
     return render(request, 'main/index.html', context)
