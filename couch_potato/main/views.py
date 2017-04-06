2ab4b5a9181f82f431cee698c7d3d4954e74a76d
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-04-06 13:54:22 -0400

    searching now applies filtered results to UI

--- couch_potato/main/views.py
+++ couch_potato/main/views.py
@@ -12,13 +12,9 @@ def detail(request, movie_id):
     return render(request, 'main/detail.html', {'movie': movie})
 
 def index(request):
+    movie_list = Movie.objects.all()    
     if(request.GET.get('submitMovie')):
         search_query = request.GET.get('search_box', None)
-        if search_query == "":
-            return
-        print(search_query)
-        results = Movie.objects.filter(Q(name__icontains=search_query))
-        print("search results:", results)
-    movie_list = Movie.objects.all()
+        movie_list = Movie.objects.filter(Q(name__icontains=search_query))
     context = {'movie_list': movie_list}
     return render(request, 'main/index.html', context)
