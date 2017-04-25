597c4db41d73499cd77ac44d0dedb9406791f585
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-04-25 15:28:02 -0400

    Apple API connection

--- couch_potato/main/apple.py
+++ couch_potato/main/apple.py
@@ -3,16 +3,26 @@
 #
 
 # Sample API call:
-# https://itunes.apple.com/search?parameterkeyvalue
+# https://itunes.apple.com/search?media=movie&limit=1&term=terminator
 
-import itunes
 import json
+import requests
 
+BASE_URL = "https://itunes.apple.com/search"
 
-def itunes_search_for_movie(movie_name):
-    search = tmdb.Search()
-    if movie_name == "":
-        return []
-    results = search.movie(query=movie_name)["results"]
-    top_three = results[:3]
-    return top_three
+def apple_search(term):
+    querystring = {"media":"movie","limit":"1","term":term}
+    response = requests.request("GET", BASE_URL, params=querystring)
+    data = json.loads(response.text)
+    
+    if data['resultCount'] == 1:
+        result = data['results'][0]
+        try:
+            price = result['trackPrice']
+            url = result['trackViewUrl']
+        except KeyError:
+            print("Call to apple API failed")
+        return (price, url)
+    else:
+        # we don't have any results for apple
+        return (0.00, "none")
--- couch_potato/main/models.py
+++ couch_potato/main/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.db.models.signals import post_init
 
 from .tmdb import search_for_movie
+from .apple import apple_search
 
 # Create your models here.
 
@@ -14,19 +15,26 @@ class Movie(models.Model):
     img_url = models.TextField(default="none")
     backdrop = models.TextField(default="none")
     
+    apple_url = models.TextField(default="none")
+    apple_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    
     def __str__(self):
         return "Name: " + self.name + " Number: " + str(self.number)
     
 def intialize_movie(**kwargs):
     instance = kwargs.get('instance')
     options = search_for_movie(instance.name)
-
+    apple_info = apple_search(instance.name)
+    
     if len(options) > 0:
         instance.description = options[0]["overview"]
         instance.number = options[0]["id"]
         instance.name = options[0]["original_title"]
         instance.img_url = options[0]["poster_path"]
         instance.backdrop = options[0]["backdrop_path"]
+
+        instance.apple_price = apple_info[0]        
+        instance.apple_url = apple_info[1]        
     else:
         instance.number = 0
         instance.description = "Unable to locate description for this movie."
